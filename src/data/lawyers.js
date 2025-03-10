a775ad23b71c39c8e7da9122599368ae2823e5d8
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-03-10 14:11:28 -0300

    sacando imagen que no anda

--- src/data/lawyers.js
+++ src/data/lawyers.js
@@ -10,7 +10,6 @@ import LawyerIMG9 from "../../public/lawyers-img/imagen-abogado-9.jpg";
 import LawyerIMG10 from "../../public/lawyers-img/imagen-abogado-10.jpg";
 import LawyerIMG11 from "../../public/lawyers-img/imagen-abogado-11.jpg";
 import LawyerIMG12 from "../../public/lawyers-img/imagen-abogado-12.jpg";
-import LawyerIMG13 from "../../public/lawyers-img/imagen-abogado-13.jpg";
 import LawyerIMG14 from "../../public/lawyers-img/imagen-abogado-14.jpg";
 
 export const arrayLawyers = [
@@ -281,30 +280,6 @@ export const arrayLawyers = [
   {
     id: "arrayLawyers-12",
     img: LawyerIMG12,
-    name: "Dawson Timms",
-    experience: "Junior Lawyer",
-    linkedin: "https://www.linkedin.com",
-    instagram: "https://www.instagram.com",
-    phone: "(54) 9 11 2458 6710",
-    email: "testemail@gmail.com",
-    cv: [
-      {
-        name: "Studies",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis quaerat, ipsum dolores rem quos fugit totam reprehenderit, commodi porro corrupti cupiditate error, optio nostrum repellendus tempora! Corporis, ratione aliquid!",
-      },
-      {
-        name: "Work Experiencie",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis quaerat, ipsum dolores rem quos fugit totam reprehenderit, commodi porro corrupti cupiditate error, optio nostrum repellendus tempora! Corporis, ratione aliquid!",
-      },
-      {
-        name: "Languages",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis quaerat, ipsum dolores rem quos fugit totam reprehenderit, commodi porro corrupti cupiditate error, optio nostrum repellendus tempora! Corporis, ratione aliquid!",
-      },
-    ],
-  },
-  {
-    id: "arrayLawyers-13",
-    img: LawyerIMG13,
     name: "Dawson Timms",
     experience: "Junior Lawyer",
     linkedin: "https://www.linkedin.com",
@@ -622,30 +597,6 @@ export const arrayLawyersSpanish = [
   {
     id: "arrayLawyers-12",
     img: LawyerIMG12,
-    name: "Dawson Timms",
-    experience: "Abogada Junior",
-    linkedin: "https://www.linkedin.com",
-    instagram: "https://www.instagram.com",
-    phone: "(54) 9 11 2458 6710",
-    email: "testemail@gmail.com",
-    cv: [
-      {
-        name: "Estudios",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis quaerat, ipsum dolores rem quos fugit totam reprehenderit, commodi porro corrupti cupiditate error, optio nostrum repellendus tempora! Corporis, ratione aliquid!",
-      },
-      {
-        name: "Experiencia Laboral",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis quaerat, ipsum dolores rem quos fugit totam reprehenderit, commodi porro corrupti cupiditate error, optio nostrum repellendus tempora! Corporis, ratione aliquid!",
-      },
-      {
-        name: "Idiomas",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis quaerat, ipsum dolores rem quos fugit totam reprehenderit, commodi porro corrupti cupiditate error, optio nostrum repellendus tempora! Corporis, ratione aliquid!",
-      },
-    ],
-  },
-  {
-    id: "arrayLawyers-13",
-    img: LawyerIMG13,
     name: "Dawson Timms",
     experience: "Abogada Junior",
     linkedin: "https://www.linkedin.com",
