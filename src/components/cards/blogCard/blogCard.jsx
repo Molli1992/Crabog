0712f42d0941fac3a9336a9fbea45dd229303e18
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-03-17 10:19:41 -0300

    transalte blog card

--- src/components/cards/blogCard/blogCard.jsx
+++ src/components/cards/blogCard/blogCard.jsx
@@ -1,22 +1,99 @@
+import React, { useState, useEffect } from "react";
 import styles from "./blogCard.module.css";
 import { FaEye } from "react-icons/fa";
+import useLanguageStore from "@/zustand/useLanguageStore";
+import { ClipLoader } from "react-spinners";
+import Swal from "sweetalert2";
 
 export default function BlogCard({ data, OnClick }) {
-  return (
-    <div className={styles.body} onClick={() => OnClick()}>
-      <button className={styles.button}>{data.type}</button>
+  const { language } = useLanguageStore();
+  const [loader, setLoader] = useState(false);
 
-      <div className={styles.flexContainer} style={{ gap: "10px" }}>
-        <p>{data.date}</p>
-        {"|"}
-        <div className={styles.flexContainer} style={{ gap: "5px" }}>
-          <FaEye className={styles.icon} />
-          <p>{data.seen}</p>
-        </div>
-      </div>
-
-      <h1 className={styles.title}>{data.title}</h1>
-      <p className={styles.desc}>{data.description}</p>
-    </div>
+  const [translateType, setTranslateType] = useState(data.type);
+  const [translateTitle, setTranslateTitle] = useState(data.title);
+  const [translateDescription, setTranslateDescription] = useState(
+    data.description
   );
+  const [formattedDate, setFormattedDate] = useState(data.date);
+
+  async function translateText(text, targetLang = "en") {
+    const res = await fetch(
+      `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
+        text
+      )}&langpair=es|${targetLang}`
+    );
+    const data = await res.json();
+    return data.responseData.translatedText;
+  }
+
+  useEffect(() => {
+    if (language !== "spanish") {
+      setLoader(true);
+
+      async function translate() {
+        try {
+          const translatedType = await translateText(data.type);
+          const translatedTitle = await translateText(data.title);
+          const translatedDesc = await translateText(data.description);
+
+          setTranslateType(translatedType);
+          setTranslateTitle(translatedTitle);
+          setTranslateDescription(translatedDesc);
+
+          function formatDate(dateString) {
+            const [day, month, yearAndTime] = dateString.split("/");
+            const [year, time] = yearAndTime.split(", ");
+            return `${month}/${day}/${year}, ${time}`;
+          }
+
+          setFormattedDate(formatDate(data.date));
+        } catch (error) {
+          Swal.fire({
+            title: "Error!",
+            text: "Error translating into English",
+            icon: "error",
+            confirmButtonText: "Ok",
+          });
+        } finally {
+          setLoader(false);
+        }
+      }
+
+      translate();
+    } else {
+      setFormattedDate(data.date);
+    }
+  }, [language]);
+
+  if (loader) {
+    return (
+      <div className={styles.body}>
+        <ClipLoader color="#192d2f" size={100} />
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.body} onClick={() => OnClick()}>
+        <button className={styles.button}>
+          {language !== "spanish" ? translateType : data.type}
+        </button>
+
+        <div className={styles.flexContainer} style={{ gap: "10px" }}>
+          <p>{language !== "spanish" ? formattedDate : data.date}</p>
+          {"|"}
+          <div className={styles.flexContainer} style={{ gap: "5px" }}>
+            <FaEye className={styles.icon} />
+            <p>{data.seen}</p>
+          </div>
+        </div>
+
+        <h1 className={styles.title}>
+          {language !== "spanish" ? translateTitle : data.title}
+        </h1>
+        <p className={styles.desc}>
+          {language !== "spanish" ? translateDescription : data.description}
+        </p>
+      </div>
+    );
+  }
 }
